4076d6bb7c252e919e739d850e0ac25728189b58
Jakub Sokołowski <jakub@status.im>
2021-02-16 18:10:22 +0100

    upgrade DigitalOcean provider to 2.5.1
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- versions.tf
+++ versions.tf
@@ -15,7 +15,7 @@ terraform {
     }
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = " = 1.22.2"
+      version = " = 2.5.1"
     }
     ansible = {
       source  = "nbering/ansible"
