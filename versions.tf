05281e05cb24b5008788908ad1c57a938680d2a0
Jakub Sokołowski <jakub@status.im>
2021-06-23 12:22:50 +0200

    upgrade Terraform to 1.0, upgrade all providers
    https://www.terraform.io/upgrade-guides/0-15.html
    https://www.terraform.io/upgrade-guides/1-0.html
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- versions.tf
+++ versions.tf
@@ -1,21 +1,9 @@
 terraform {
-  required_version = "~> 0.14.4"
+  required_version = "~> 1.0.0"
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = " = 2.10.1"
-    }
-    google = {
-      source  = "hashicorp/google"
-      version = " = 3.42.0"
-    }
-    alicloud = {
-      source  = "aliyun/alicloud"
-      version = " = 1.95.0"
-    }
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = " = 2.5.1"
+      version = " = 2.21.0"
     }
     ansible = {
       source  = "nbering/ansible"
@@ -24,6 +12,9 @@ terraform {
     pass = {
       source  = "camptocamp/pass"
       version = " = 2.0.0"
+    }
+    google = {
+      source  = "hashicorp/google"
     }
   }
 }
